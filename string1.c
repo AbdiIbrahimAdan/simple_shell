e802763cd748e716cae8057e912032403ddd0ae7
Charles Mwaniki <mwanikigachanja@gmail.com>
2023-10-18 09:42:13 +0300

    Buffer error sorted

--- string1.c
+++ string1.c
@@ -1,49 +1,55 @@
 #include "shell.h"
+
 /**
- * string_copy - copies string
- * @destination:destination
- * @source:source
- * Return:pointer to the destination
+ * _strcpy - copies a string
+ * @dest: the destination
+ * @src: the source
+ *
+ * Return: pointer to destination
  */
-char *string_copy(char *destination, char *source)
+char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
-	if (destination == source || source == 0)
-		return (destination);
-	while (source[i])
+	if (dest == src || src == 0)
+		return (dest);
+	while (src[i])
 	{
-		destination[i] = source[i];
+		dest[i] = src[i];
 		i++;
 	}
-	destination[i] = 0;
-	return (destination);
+	dest[i] = 0;
+	return (dest);
 }
+
 /**
- * _strdup - duplicates string
- * @str:string to duplicate
- * Return:pointer to the duplicated string
+ * _strdup - duplicates a string
+ * @str: the string to duplicate
+ *
+ * Return: pointer to the duplicated string
  */
 char *_strdup(const char *str)
 {
 	int length = 0;
-	char *result;
+	char *ret;
 
 	if (str == NULL)
 		return (NULL);
 	while (*str++)
 		length++;
-	result = malloc(sizeof(char) * (length + 1));
-	if (!result)
+	ret = malloc(sizeof(char) * (length + 1));
+	if (!ret)
 		return (NULL);
 	for (length++; length--;)
-		result[length] = *--str;
-	return (result);
+		ret[length] = *--str;
+	return (ret);
 }
+
 /**
- * _puts - prints input string
- * @str:the string to be printed
- * Return:nothing
+ * _puts - prints an input string
+ * @str: the string to be printed
+ *
+ * Return: Nothing
  */
 void _puts(char *str)
 {
@@ -57,23 +63,25 @@ void _puts(char *str)
 		i++;
 	}
 }
+
 /**
- * _putchar - write character c to the stdout
- * @c:character to print
- * Return:on success1, on error -1 .
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
 {
 	static int i;
-	static char buffer[WRITE_BUFFER_SIZE];
+	static char buf[WRITE_BUF_SIZE];
 
-	if (c == BUFFER_FLUSH || i >= WRITE_BUFFER_SIZE)
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
 	{
-		write(1, buffer, i);
+		write(1, buf, i);
 		i = 0;
 	}
-	if (c != BUFFER_FLUSH)
-		buffer[i++] = c;
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
 	return (1);
 }
-
